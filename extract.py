b829b523b3d97cfa068c94aab5a10ac4c3ad109e
Vijay Raj Saravanan Radhakrishnan <vijay_saravanan@outlook.com>
2019-07-14 12:13:43 -0400

    Feat: Access to reddit successful.

--- extract.py
+++ extract.py
@@ -10,3 +10,10 @@ client_id = config_parser['REDDIT']['CLIENT_ID']
 username = config_parser['REDDIT']['USERNAME']
 password = config_parser['REDDIT']['PASSWORD']
 
+reddit_client = praw.Reddit(client_id=client_id,
+                            client_secret=client_secret,
+                            username=username,
+                            password=password,
+                            user_agent='testscript by /u/dash_365'
+                            )
+print(reddit_client.user.me())
