16990716d4248a1afe21fea6d5aa3661170486a4
Vijay Raj Saravanan Radhakrishnan <vijay_saravanan@outlook.com>
2019-07-14 13:18:05 -0400

    Feat: Extract post details from API and store as CSV file using get_posts()
    Attributes extracted : id, title, number of comments, score, upvote ratio, created utc, url

--- extract.py
+++ extract.py
@@ -1,6 +1,6 @@
 import praw
 import configparser
-import os
+import pandas as pd
 
 def connect_to_reddit(configuration_file):
     config_parser = configparser.ConfigParser()
@@ -25,5 +25,36 @@ def connect_to_reddit(configuration_file):
     if returned_user_name == username:
         print("Connection successful")
         return reddit_client
+
+
+def get_posts(reddit_client, num_posts=1):
+    posts = []
+    column_names = ['id', 'title', 'num_comments',
+                    'score', 'upvote_ratio', 'created_utc',
+                    'url']
+    hot_posts = reddit_client.subreddit('all').top('day', limit=num_posts)
+    print(f'Obtained Submission object with top {num_posts} comments.')
+    for count, post in enumerate(hot_posts, start=1):
+        print(f'Processing {count} of {num_posts}.')
+        posts.append([post.id,
+                      post.title,
+                      post.num_comments,
+                      post.score,
+                      post.upvote_ratio,
+                      post.created_utc,
+                      post.url
+                      ])
+    print("Process completed successfully.\nWriting data to CSV format.")
+    df = pd.DataFrame(posts, columns=column_names)
+    df.to_csv('post_details.csv')
+    print("CSV file stored.")
+
+
+if __name__ == '__main__':
+    reddit_client = connect_to_reddit('configurations.cfg')
+    get_posts(reddit_client, 5)
+
+
+
 
 
